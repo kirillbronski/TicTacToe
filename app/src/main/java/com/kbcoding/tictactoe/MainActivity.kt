0945127f092c5bcef9dd05d20a446c7faf280656
Kirill <kirillbronski@gmail.com>
2023-08-21 17:15:39 +0300

    test launch of the application with random filling of the dimensions of the playing field and the marks of the players by pressing the button

--- app/src/main/java/com/kbcoding/tictactoe/MainActivity.kt
+++ app/src/main/java/com/kbcoding/tictactoe/MainActivity.kt
@@ -2,10 +2,33 @@ package com.kbcoding.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kbcoding.tictactoe.databinding.ActivityMainBinding
+import com.kbcoding.tictactoe.presentation.utils.Cell
+import com.kbcoding.tictactoe.presentation.utils.TicTacToeField
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cvTicTacToeView.ticTacToeField = TicTacToeField(10, 10)
+        binding.btnRandomField.setOnClickListener {
+            binding.cvTicTacToeView.ticTacToeField = TicTacToeField(
+                Random.nextInt(3, 10),
+                Random.nextInt(3, 10)
+            )
+            for (i in 0..binding.cvTicTacToeView.ticTacToeField!!.rows) {
+                for (j in 0..binding.cvTicTacToeView.ticTacToeField!!.columns) {
+                    if (Random.nextBoolean()){
+                        binding.cvTicTacToeView.ticTacToeField!!.setCell(i,j, Cell.PLAYER_ONE)
+                    } else {
+                        binding.cvTicTacToeView.ticTacToeField!!.setCell(i,j, Cell.PLAYER_TWO)
+                    }
+                }
+            }
+        }
     }
 }
